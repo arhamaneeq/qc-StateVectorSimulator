6b19908c3ffa578f9374ebc0c4a835c89efbe6c4
Arham Aneeq <arhamaneeq18@gmail.com>
2025-09-29 12:58:08 +0530

    Implemented multiplication for matrix objects
    - Matmul (overloads *)
    - Kronecker (overloads ^)

--- src/matrix.rs
+++ src/matrix.rs
@@ -24,8 +24,49 @@ impl Matrix {
 impl std::ops::Mul<Matrix> for Matrix {
     type Output = Matrix;
 
-    fn mul(self, _rhs: Matrix) -> Matrix {
-        // TODO: create accessors for matrix
+    fn mul(self: Matrix, _rhs: Matrix) -> Matrix {
+        assert_eq!(self.width, _rhs.height, "Dimension mismatch!");
+
+        let mut result: Matrix = Matrix::new([self.height, _rhs.width]);
+
+        for i in 0..self.height {
+            for j in 0.._rhs.width {
+                let mut val: Complex = Complex::new(0.0, 0.0);
+
+                for k in 0..self.width {
+                    val = val + self[(i, k)] * _rhs[(k, j)];
+                }
+
+                result[(i, j)] = val;
+            }
+        }
+
+        result
+    }
+}
+
+impl std::ops::BitXor<Matrix> for Matrix {
+    type Output = Matrix;
+
+    fn bitxor(self, _rhs: Matrix) -> Self::Output {
+        let m: usize = self.height;
+        let n: usize = self.width;
+        let p: usize = _rhs.height;
+        let q: usize = _rhs.width;
+
+        let mut result: Matrix = Matrix::new([m * p, n * q]);
+
+        for i in 0..m - 1 {
+            for j in 0..n - 1 {
+                for k in 0..p - 1 {
+                    for l in 0..q - 1 {
+                        result[(i * p + k, j * q + l)] = self[(i, j)] * _rhs[(k, l)];
+                    }
+                }
+            }
+        }
+
+        result
     }
 }
 
@@ -34,5 +75,11 @@ impl std::ops::Index<(usize, usize)> for Matrix {
 
     fn index(&self, (i, j): (usize, usize)) -> &Self::Output {
         &self.matrix[i][j]
+    }
+}
+
+impl std::ops::IndexMut<(usize, usize)> for Matrix {
+    fn index_mut(&mut self, (i, j): (usize, usize)) -> &mut Self::Output {
+        &mut self.matrix[i][j]
     }
 }
